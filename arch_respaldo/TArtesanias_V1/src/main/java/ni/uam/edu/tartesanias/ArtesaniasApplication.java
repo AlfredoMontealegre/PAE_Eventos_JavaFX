92ab51843b7dcadb532266577ec49c6dbd9c2be3
AlfredoMontealegre <montealfredo24@gmail.com>
2026-09-02 14:57:59 -0600

    add: ArchivoRespaldo para TArtesanias Original
    Por si resulta algun problema con el TArtesanias pubicado en main, se creo un respaldo en esta carpeta como emergencias.
    - Diferencias: El codigo esta ordenado como la version anterior a la final

---
+++ arch_respaldo/TArtesanias_V1/src/main/java/ni/uam/edu/tartesanias/ArtesaniasApplication.java
@@ -0,0 +1,34 @@
+package ni.uam.edu.tartesanias;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ArtesaniasApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                ArtesaniasApplication.class.getResource(
+                        "tienda-view.fxml"
+                )
+        );
+
+        Scene scene = new Scene(
+                fxmlLoader.load(),
+                900, 650
+        );
+
+        stage.setTitle("Artesanías Güegüenses");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
